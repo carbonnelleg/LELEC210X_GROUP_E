35b86983e8ffd999a4d896b265c9c89909c894f7
XenoCrafts <victor-emmanuel.bertrand@student.uclouvain.be>
2025-03-07 12:21:37 +0100

    CNN added to the whole chain (need to be tested)

--- auth/src/auth/model_prediction.py
+++ auth/src/auth/model_prediction.py
@@ -3,8 +3,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pickle
 import sklearn
+import tensorflow as tf
 
-def model_prediction(payload):
+classnames = ['chainsaw', 'fire', 'fireworks', 'gun']
+def decision_maxlikelihood(probs):
+    """Règle du maximum de vraisemblance : Choisir la classe avec le produit maximal des probabilités."""
+    probs = np.array(probs)
+    prod_probs = np.prod(probs, axis=0)
+    return classnames[np.argmax(prod_probs)]
+
+
+def old_model_prediction(payload):
     this_fv = np.frombuffer(payload, dtype=np.uint16)
     # fm_dir = "data/feature_matrices/"  # where to save the features matrices
     filename = "auth/src/auth/model.pickle"
@@ -17,3 +26,19 @@ def model_prediction(payload):
     mat[0] = this_fv
     prediction = model.predict(mat)
     return prediction[0]
+
+old_predictions = []
+model = tf.keras.models.load_model("auth/src/auth/CNN_model.keras")
+def model_prediction(payload):
+    this_fv = np.frombuffer(payload, dtype=np.uint16)
+    mat = np.zeros((2, len(this_fv)))
+    this_fv = this_fv / np.linalg.norm(this_fv)
+    mat[0] = this_fv
+    prediction = model.predict(mat)
+    if len(old_predictions) < 4:
+        old_predictions.append(prediction[0])
+    if len(old_predictions) == 4:
+        old_predictions.pop(0)
+        old_predictions.append(prediction[0])
+    return decision_maxlikelihood(old_predictions)
+        
